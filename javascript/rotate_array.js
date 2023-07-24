892e0184faf4cf8d595d258b816cfd14d3c25850
matthew rendely <matthew.rendely@gmail.com>
2023-07-23 20:10:47 -0700

    Solved

--- javascript/rotate_array.js
+++ javascript/rotate_array.js
@@ -1,5 +1,8 @@
 function rotateArray(arr, k) {
-  // type your code here
+  const remainder = k % arr.length;
+  const offset = arr.length - remainder;
+  if (remainder == 0) return arr 
+  return [...arr.slice(offset), ...arr.slice(0,offset)]
 }
 
 if (require.main === module) {
@@ -16,6 +19,9 @@ if (require.main === module) {
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", rotateArray([1, 2, 3], 3));
+
+  console.log("Expecting: [1, 2, 3]");
+  console.log("=>", rotateArray([1, 2, 3], 6));
 }
 
 module.exports = rotateArray;
